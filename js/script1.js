73b2089a875ef835f68abf6db7ae8cf1914f7eb6
Bárbara Gabriela <barbaragabi15@hotmail.com>
2021-11-22 20:15:44 -0300

    editar e deletar ok

--- js/script1.js
+++ js/script1.js
@@ -2,19 +2,80 @@ class Produto {
     constructor() {
         this.id = 1;
         this.arrayProdutos = [];
+        this.editId = null;
 
     }
     salvar() {
         let produto = this.lerDados();
-        if (this.validaCampos(produto) == true){
-            this.adicionar(produto);
+        if (this.validaCampos(produto) == true) {
+            if (this.editId == null) {
+                this.adicionar(produto);
+            }
+            else{
+                this.atualizar(this.editId, produto);
+            }
         }
-        console.log(this.arrayProdutos);
+
+        this.listaTabela();
+        this.cancelar();
     }
-    adicionar(produto){
+
+    listaTabela() {
+        let tbody = document.getElementById('tbody');
+        tbody.innerText = '';
+
+        for (let i = 0; i < this.arrayProdutos.length; i++) {
+            let tr = tbody.insertRow(); //insertRom - cria uma nova linha dentro da tabela.
+            let td_id = tr.insertCell();//insereCell insere uma nova coluna e add essa coluna ao td_id
+            let td_produtos = tr.insertCell();
+            let td_valor = tr.insertCell();
+            let td_acoes = tr.insertCell();
+
+            td_id.innerText = this.arrayProdutos[i].id;
+            td_produtos.innerText = this.arrayProdutos[i].nomeProduto;
+            td_valor.innerText = this.arrayProdutos[i].preco;
+
+            td_id.classList.add('center');
+
+            let imgEdit = document.createElement('img');
+            imgEdit.src = 'img/editar.png';
+            imgEdit.setAttribute("onclick", "produto.editar(" + JSON.stringify(this.arrayProdutos[i]) + ")");
+
+
+            let imgDelet = document.createElement('img');
+            imgDelet.src = 'img/deletar-lixeira.png';
+            imgDelet.setAttribute("onclick", "produto.deletar(" + this.arrayProdutos[i].id + ")");
+
+            td_acoes.appendChild(imgEdit);
+            td_acoes.appendChild(imgDelet);
+
+            console.log(this.arrayProdutos);
+        }
+    }
+
+    adicionar(produto) {
+        produto.preco = parseFloat(produto.preco);
         this.arrayProdutos.push(produto);
-        this.id ++;
+        this.id++;
     }
+
+    atualizar(id, produto){
+        for(let i = 0; i < this.arrayProdutos.length; i++){
+            if(this.arrayProdutos[i].id == id){
+                this.arrayProdutos[i].nomeProduto = produto.nomeProduto;
+                this.arrayProdutos[i].preco = produto.preco;
+            }
+        }
+    }
+
+    editar(dados) {
+        this.editId = dados.id;
+        document.getElementById('produto').value = dados.nomeProduto;
+        document.getElementById('preco').value = dados.preco;
+        document.getElementById('btn1').innerText = "atualizar";
+
+    }
+
     lerDados() {
         let produto = {}
         produto.id = this.id;
@@ -24,13 +85,14 @@ class Produto {
 
         return produto;
     }
+
     validaCampos(produto) {
         let msg = '';
 
         if (produto.nomeProduto == '') {
             msg += 'Informe o nome do produto \n';
         }
-        if (produto.preco == ''){
+        if (produto.preco == '') {
             msg += 'Informe o preço do produto \n';
         }
 
@@ -40,7 +102,28 @@ class Produto {
         }
         return true;
     }
+
     cancelar() {
+        document.getElementById('produto').value = '';
+        document.getElementById('preco').value = '';
+        document.getElementById('btn1').innerText = "salvar";
+        this.editId = null;
+    }
+
+    deletar(id) {
+
+
+        if (confirm("Deseja realmente deletar o produto?")) {
+            let tbody = document.getElementById('tbody');
+
+            for (let i = 0; i < this.arrayProdutos.length; i++) {
+                if (this.arrayProdutos[i].id == id) {
+                    this.arrayProdutos.splice(i, 1);
+                    tbody.deleteRow(i)
+                }
+            }
+
+        }
 
     }
 }
